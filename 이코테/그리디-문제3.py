d2a1e85968b9883359403c4ae9d7dfbb778e649c
JIHO KIM <kimjiho1125a@naver.com>
2021-07-17 03:36:09 +0900

    그리디 알고리즘 예제 3번

---
+++ 이코테/그리디-문제3.py
@@ -0,0 +1,19 @@
+n = int(input())
+fear = list(map(int,input().split()))
+fear.sort()
+
+result = 0 #총 그룹의 수
+count = 0 #현재 그룹에 포함된 모험가의 수
+
+12223
+
+for i in data: #공포도를 낮은 것부터 하나씩 확인하며
+    count += 1 #현재 그룹에 해당 모험가를 포함시키기
+    if count >= i : #현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹결성
+        result += 1 #총 그룹의 수 증가시키기
+        count = 0 #현재 그룹에 포함된 모험가의 수 초기화
+
+print(result)
+
+
+
