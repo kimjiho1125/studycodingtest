8db3779846d5cc999a36afd2e1e08c9f503179b4
kimjiho1125 <kimjiho1125a@naver.com>
2022-07-20 01:30:13 +0900

    백준 11650번

---
+++ 백준/백준 11650번.py
@@ -0,0 +1,17 @@
+import sys
+
+N = int(sys.stdin.readline())
+numbers = []
+
+for _ in range(N):
+    x,y = map(int,sys.stdin.readline().split())
+    numbers.append((x,y))
+
+numbers.sort(key=lambda x:x[1])
+numbers.sort(key=lambda x:x[0])
+
+for i in numbers:
+    print(i[0], i[1])
+
+
+    
