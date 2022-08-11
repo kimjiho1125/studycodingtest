2fd43ea38f264aeba127d9cfa5e688e9caeee272
kimjiho1125 <kimjiho1125a@naver.com>
2022-08-11 23:56:36 +0900

    백준 11659번.py

--- 백준/백준 11659번.py
+++ 백준/백준 11659번.py
@@ -1,3 +1,17 @@
 import sys
+input = sys.stdin.readline
 
-N, M = sys.stdin.readline().split()
+N, M = map(int, input().split())
+numList = list(map(int,input().split()))
+prefix_sum = [0]
+
+temp = 0
+
+for i in numList:
+    temp += i
+    prefix_sum.append(temp)
+
+for i in range(M):
+    index1, index2 = map(int,input().split())
+    print(prefix_sum[index2]-prefix_sum[index1-1])
+    
